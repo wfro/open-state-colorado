a0cb99cca501592af69282ceaf8ca6bc2f27dd5d
Will Faurot <wfaurot@gmail.com>
2014-10-05 11:42:41 -0600

    Add tests for query string methods

--- app/models/open_states.rb
+++ app/models/open_states.rb
@@ -2,21 +2,26 @@ class OpenStates
   BASE_URL = "http://openstates.org/api/v1/"
   API_KEY = ENV["open_states_api_key"]
 
-  attr_reader :district
-
-  def initialize(district)
-    @district = district
-  end
-
-  def legislators_by_district_search_url
-    "legislators/?" + {
+  def legislators_by_district_search_string(district)
+    url = "legislators/?" + {
       apikey: API_KEY,
       district: district,
       state: "co"
     }.to_query
   end
 
-  def open_states_connection
+  def legislators_search_string
+    url = "legislators/?" + {
+      apikey: API_KEY,
+      state: "co"
+    }.to_query
+  end
+
+  def legislators_by_id_search_string(id)
+    url = "legislators/" + id + "/?apikey=#{API_KEY}"
+  end
+
+  def connection
     return Faraday.new(url: BASE_URL) do |faraday|
       faraday.request :url_encoded
       faraday.response :logger
--- spec/models/open_states_spec.rb
+++ spec/models/open_states_spec.rb
@@ -1,19 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe OpenStates, type: :model do
-  describe "#legislators_by_district_search_url" do
-    it "returns an appropriate query string" do
+  describe "#legislators_by_district_search_string" do
+    it "returns correct query string" do
       district = 1
-      open_states = OpenStates.new(district)
-      result = open_states.legislators_by_district_search_url
+      open_states = OpenStates.new
+      result = open_states.legislators_by_district_search_string(1)
       expected = "legislators/?apikey=bfef33e0a83e4e98931765048ca24dd6&district=1&state=co"
       expect(result).to eq expected
     end
   end
 
-  describe "#open_states_connection" do
+  describe "#legislators_search_string" do
+    it "returns correct query string" do
+      open_states = OpenStates.new
+      result = open_states.legislators_search_string
+      expected = "legislators/?apikey=bfef33e0a83e4e98931765048ca24dd6&state=co"
+      expect(result).to eq expected
+    end
+  end
+
+  describe "#legislators_by_id_search_string" do
+    it "returns correct query string" do
+      open_states = OpenStates.new
+      result = open_states.legislators_by_id_search_string("12345")
+      expected = "legislators/12345/?apikey=bfef33e0a83e4e98931765048ca24dd6"
+    end
+  end
+
+  describe "#connection" do
     it "returns a Faraday connection object" do
-      result = OpenStates.new(1).open_states_connection
+      result = OpenStates.new.connection
       expect(result.class).to eq Faraday::Connection
     end
   end

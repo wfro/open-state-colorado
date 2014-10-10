050213ef451f75c6017bd69875ff567ecdcb3b15
Will Faurot <wfaurot@gmail.com>
2014-10-09 20:21:42 -0600

    Add Bill#save_data_from_api
    This is still using a custom create, seems reasonable to find
    another solution and let ActiveRecord handle create/update.
    Possible option could be to add some logic to trim the incoming
    JSON and pass that into #create.

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -2,4 +2,13 @@ class Bill < ActiveRecord::Base
   validates :external_id, presence: true
   has_many :votes
   has_many :actions
+
+  def self.save_data_from_api(data = {})
+    create! do |bill|
+      bill.external_id = data["id"]
+      bill.state = data["co"]
+      bill.title = data["title"]
+      bill.type = data["type"]
+    end
+  end
 end

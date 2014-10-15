7ab93c4df54e9ccb32116165875473fc02bb6d95
Will Faurot <wfaurot@gmail.com>
2014-10-15 15:36:35 -0600

    Fix legislator spec

--- spec/models/legislator_spec.rb
+++ spec/models/legislator_spec.rb
@@ -21,18 +21,4 @@ RSpec.describe Legislator, :type => :model do
   it { should respond_to(:url) }
   it { should respond_to(:full_name) }
   it { should be_valid }
-
-  describe ".filter_attributes" do
-    it "returns a hash filtered to only necessary cols from legislators table" do
-      initial_data = {"leg_id" => "12345", "district" => "1", "party" => "donkeys",
-                      "email" => "johndoe@example.com", "photo_url" => "", "url" => "",
-                      "full_name" => "John Doe", "chamber" => "upper"}
-      filtered = Legislator.filter_attributes(initial_data)
-      expected_attr = %w[district party email photo_url url full_name external_id]
-
-      expect(filtered.keys).to eq expected_attr
-      expect(filtered.keys).not_to include "chamber"
-    end
-  end
-
 end

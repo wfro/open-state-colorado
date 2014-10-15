fb2052ddd5efc07019f68f858bab455e263a396d
Will Faurot <wfaurot@gmail.com>
2014-10-15 13:01:29 -0600

    Scaffold for rake task tests

---
+++ spec/tasks/api_tasks_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+describe "external api tasks" do
+  describe "api:legislators" do
+    xit "saves legislators" do
+    end
+  end
+
+  describe "api:bills" do
+    xit "saves bills and votes" do
+    end
+  end
+end
